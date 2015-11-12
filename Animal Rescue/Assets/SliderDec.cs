445a4262d931e5d69b54b45813144abac16685d1
eschreibman <eliza.schreibman@gmail.com>
2015-11-12 11:52:38 -0500

    prototype

---
+++ Animal Rescue/Assets/SliderDec.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SliderDec : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+
+		float n = (Time.time * 0.1f);
+		float dec = n * (Mathf.Log (1+n));
+
+		Slider sMeter = GameObject.Find("SuspicionSlider").GetComponent<Slider>();
+		sMeter.value = dec;
+
+	}
+}
