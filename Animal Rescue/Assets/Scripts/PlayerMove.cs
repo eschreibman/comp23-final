eb23ff38a2800e97c0c5e9c2d13448f5f91d961d
eschreibman <eliza.schreibman@gmail.com>
2015-11-16 12:48:52 -0500

    joystick movement. stealth button changes material.

---
+++ Animal Rescue/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerMove : MonoBehaviour {
+	public float speed = 4;
+	bool inStealth = false;
+	public Material regMat;
+	public Material stealthMat;
+	
+	public Renderer rend;
+
+	
+	void Start() {
+		rend = GetComponent<Renderer>();
+		rend.enabled = true;
+	}
+
+	void Update () {
+
+		CharacterController controller = GetComponent<CharacterController>();
+
+		//move forward and backwards
+		Vector3 forward = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal") * speed, 0, 
+		                              CrossPlatformInputManager.GetAxis("Vertical") * speed);
+		inStealth = CrossPlatformInputManager.GetButton("Stealth");
+		if(inStealth == true){
+			// Change renderer's material 	
+			// assign it to the renderer
+			rend.sharedMaterial = stealthMat;
+		} else {
+			rend.sharedMaterial = regMat;
+		}
+		controller.SimpleMove(forward);
+	}
+}
