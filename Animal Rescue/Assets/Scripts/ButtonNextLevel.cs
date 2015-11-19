2de092ce08813ac9bd8e48dccc459a93143ae83a
eschreibman <eliza.schreibman@gmail.com>
2015-11-19 11:41:36 -0500

    Created and linked title screen and rest of game

---
+++ Animal Rescue/Assets/Scripts/ButtonNextLevel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonNextLevel : MonoBehaviour 
+{
+	public Rect button = new Rect(15,15,200,110);
+	public string buttonLabel = "Start game";
+	public string levelToLoad;
+
+	public void OnGUI(){
+		if (GUI.Button(button, buttonLabel))
+			Application.LoadLevel(levelToLoad);
+	}
+	/*public void NextLevelButton(int index)
+	{
+		Application.LoadLevel(index);
+	}
+	
+	public void NextLevelButton(string levelName)
+	{
+		Debug.Log("Load " + levelName + "scene");
+		Application.LoadLevel(levelName);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("Load start scene");
+		Application.LoadLevel("Start");
+	}
+*/
+}
--- Animal Rescue/Assets/Scripts/SliderDec.cs
+++ Animal Rescue/Assets/Scripts/SliderDec.cs
@@ -4,10 +4,15 @@ using UnityEngine.UI;
 
 public class SliderDec : MonoBehaviour {
 
+	public float speed = 0.05f;
+	void Start (){
+
+
+	}
 	// Update is called once per frame
 	void Update () {
 
-		float n = (Time.time * 0.1f);
+		float n = (Time.timeSinceLevelLoad * speed);
 		float dec = n * (Mathf.Log (1+n));
 
 		Slider sMeter = GameObject.Find("SuspicionSlider").GetComponent<Slider>();
