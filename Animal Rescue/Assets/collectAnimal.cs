d169dcad9cc8b93a13801cf516fc9c4f78e6c5fa
eschreibman <eliza.schreibman@gmail.com>
2015-12-03 02:34:06 -0500

    basic collisions working

---
+++ Animal Rescue/Assets/collectAnimal.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collectAnimal : MonoBehaviour {
+	public Material matA;
+	public Material matB;
+
+	public Renderer rend;
+
+	void Start() {
+	/*	rend = GetComponent<Renderer>();
+		rend.enabled = true;
+		rend.sharedMaterial = matA;
+*/
+	}
+
+	void OnTriggerEnter(Collider other){
+		Debug.Log("colliding");
+		if(other.gameObject.CompareTag("Cat")){
+			other.gameObject.SetActive(false);
+		}
+	/*	if(col.gameObject.name == "Player"){
+
+			rend.sharedMaterial = matB;
+
+		}
+		else{
+			rend.sharedMaterial = matA;
+		}*/
+	}
+	void OnTriggerExit(Collider other){
+		Debug.Log ("not colliding");
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
