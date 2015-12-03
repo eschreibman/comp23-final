ca588e13f29659938ada1dd7696d30c4a279c713
eschreibman <eliza.schreibman@gmail.com>
2015-12-03 11:20:09 -0500

    pickup pet GUI working. can only pickup one pet at a time now

--- Animal Rescue/Assets/collectAnimal.cs
+++ Animal Rescue/Assets/collectAnimal.cs
@@ -1,38 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class collectAnimal : MonoBehaviour {
-	public Material matA;
-	public Material matB;
-
-	public Renderer rend;
+	private bool carryingPet;
+	private Text petText;
+	private Color defaultColor;
+	private Color withPetColor;
+	private GameObject stick;
+	private GameObject inventory;
 
 	void Start() {
-	/*	rend = GetComponent<Renderer>();
-		rend.enabled = true;
-		rend.sharedMaterial = matA;
-*/
+		carryingPet = false;
+		defaultColor = new Color(1,0,0,1);
+		withPetColor = new Color(0,1,0,1);
+		stick = GameObject.Find("MobileSingleStickControl");
+		inventory = stick.transform.Find("Pet Inventory").gameObject;
+		petText = inventory.GetComponentInChildren<Text>();
 	}
 
 	void OnTriggerEnter(Collider other){
-		Debug.Log("colliding");
-		if(other.gameObject.CompareTag("Cat")){
-			other.gameObject.SetActive(false);
+		//player will "pick up" pet if they collide and they are not already carrying a pet
+		if(other.gameObject.CompareTag("Cat") && !carryingPet){
+			carryingPet = true;
+			//other.gameObject.SetActive(false);
+			Destroy(other.gameObject);
 		}
-	/*	if(col.gameObject.name == "Player"){
-
-			rend.sharedMaterial = matB;
-
-		}
-		else{
-			rend.sharedMaterial = matA;
-		}*/
 	}
 	void OnTriggerExit(Collider other){
 		Debug.Log ("not colliding");
 	}
 	// Update is called once per frame
 	void Update () {
-	
+		if(carryingPet){
+			petText.color = withPetColor;
+		}
+		else {
+			petText.color = defaultColor;
+		}
 	}
 }
